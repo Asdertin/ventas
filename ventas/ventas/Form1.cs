74b74eef9acc9d37781e42ee81eda7e27ef18c99
Jaime Toledo <jaime.toledo3012@gmail.com>
2021-09-21 12:13:28 -0600

    Creando el programa hola mundo

--- ventas/ventas/Form1.cs
+++ ventas/ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("Hasta las puertas mas pesadas pueden ser abiertas");
         }
     }
 }
